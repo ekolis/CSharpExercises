c2b848a7a7f0c0d2260abcedd03cc3c812e91ace
Ed Kolis <edkolis@gmail.com>
2022-01-03 18:46:41 -0500

    Added while loop exercises

--- CSharpExercises/Program.cs
+++ CSharpExercises/Program.cs
@@ -11,6 +11,21 @@ Console.WriteLine($"42 is {IfExercise.Modulo3(42)}"); // should be "divisible by
 Console.WriteLine($"7 is {IfExercise.Modulo3(7)}"); // should be "remainder one"
 Console.WriteLine($"88 is {IfExercise.Modulo3(88)}"); // should be "remainder two"
 Console.WriteLine();
+Console.WriteLine("--- While Exercises ---");
+Console.WriteLine($"The first lowercase letter in cat is {WhileExercise.FirstLowercaseLetter("cat")}"); // should be 'c'
+Console.WriteLine($"The first lowercase letter in Fred is {WhileExercise.FirstLowercaseLetter("Fred")}"); // should be 'r'
+Console.WriteLine($"The first lowercase letter in iPod is {WhileExercise.FirstLowercaseLetter("iPod")}"); // should be 'i'
+Console.WriteLine($"The first lowercase letter in XMLHttpRequest is {WhileExercise.FirstLowercaseLetter("XMLHttpRequest")}"); // should be 't'
+Console.WriteLine();
+Console.WriteLine($"The sum of the digits of 123 is {WhileExercise.SumOfDigits(123)}"); // should be 6
+Console.WriteLine($"The sum of the digits of 666 is {WhileExercise.SumOfDigits(666)}"); // should be 18
+Console.WriteLine($"The sum of the digits of 1248 is {WhileExercise.SumOfDigits(666)}"); // should be 15
+Console.WriteLine();
+Console.WriteLine($"How many consecutive capital letters does XML start with? There are {WhileExercise.ConsecutiveCapitalLetterCount("XML")}"); // should be 3
+Console.WriteLine($"How many consecutive capital letters does California start with? There are {WhileExercise.ConsecutiveCapitalLetterCount("California")}"); // should be 1
+Console.WriteLine($"How many consecutive capital letters does iOS start with? There are {WhileExercise.ConsecutiveCapitalLetterCount("iOS")}"); // should be 0
+Console.WriteLine($"How many consecutive capital letters does JSONSerializer start with? There are {WhileExercise.ConsecutiveCapitalLetterCount("JSONSerializer")}"); // should be 5
+Console.WriteLine();
 Console.WriteLine("--- For Exercises ---");
 Console.WriteLine($"Let's count up to 3! {ForExercise.CountUp(3)}"); // should be "123"
 Console.WriteLine();
---
+++ CSharpExercises/WhileExercise.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExercises
+{
+	internal static class WhileExercise
+	{
+		public static char FirstLowercaseLetter(string text)
+		{
+			// TODO: return the first lowercase letter in the string
+			return '?';
+		}
+
+		public static int SumOfDigits(int num)
+		{
+			// TODO: return the sum of the digits of the number
+			return -999;
+		}
+
+		public static int ConsecutiveCapitalLetterCount(string word)
+		{
+			// TODO: return the number of consecutive capital letters in the word, starting from the first letter
+			return -999;
+		}
+	}
+}
